9c905f82cfc4137fe81f99d181e3310d3d479b48
Ken Talley <ktalley@figure.com>
2021-10-27 04:11:15 -0600

    update json schema example to use BidOrderV2 and ContractInfoV3

--- examples/schema.rs
+++ examples/schema.rs
@@ -4,8 +4,8 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use ats_smart_contract::ask_order::{AskOrderClass, AskOrderStatus, AskOrderV1};
-use ats_smart_contract::bid_order::BidOrderV1;
-use ats_smart_contract::contract_info::ContractInfoV2;
+use ats_smart_contract::bid_order::BidOrderV2;
+use ats_smart_contract::contract_info::ContractInfoV3;
 use ats_smart_contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use ats_smart_contract::version_info::VersionInfoV1;
 
@@ -18,8 +18,8 @@ fn main() {
     export_schema(&schema_for!(AskOrderV1), &out_dir);
     export_schema(&schema_for!(AskOrderClass), &out_dir);
     export_schema(&schema_for!(AskOrderStatus), &out_dir);
-    export_schema(&schema_for!(BidOrderV1), &out_dir);
-    export_schema(&schema_for!(ContractInfoV2), &out_dir);
+    export_schema(&schema_for!(BidOrderV2), &out_dir);
+    export_schema(&schema_for!(ContractInfoV3), &out_dir);
     export_schema(&schema_for!(VersionInfoV1), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
